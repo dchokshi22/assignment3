bd4194f78d1e66e7894fc7bd00c28672091e3184
Divya Chokshi <divya@cloudflare.com>
2024-04-29 10:26:07 -0500

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,32 @@
  * - Run `npm run deploy` to publish your worker
  *
  * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx) {
 		return new Response('Hello World!');
 	},
+}; 
+*/
+
+var src_default= {
+	async fetch(request, env, ctx) {
+console.log('Logging: '+ request.url)
+if(request.method == 'POST') {
+      return new Response('Hello World!This is POST method', {
+       headers: {
+     'Content-Type':'application/json',
+         },
+    });
+}
+else{
+return new Response('Only POST method allowed',{
+        headers:{    
+              'Content-Type': 'application/json',
+                },
+     });
+   }
+}
 };
+export {
+src_default as default
+	};
