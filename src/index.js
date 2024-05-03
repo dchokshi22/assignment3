2ecbffc291885b4185f015684a115b1474e1b465
Divya Chokshi <divya@cloudflare.com>
2024-05-03 16:03:44 -0500

    assignment 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
@@ -35,3 +36,51 @@ return new Response('Only POST method allowed',{
 export {
 src_default as default
 	};
+=======
+// src/index.js
+var src_default= {
+	async fetch(request, env, ctx) {
+  console.log("Logging:" + request.url) //logging request URL
+  
+  
+  const fruit_table = {                 //created lookup table  
+	1 : "Apple",
+	2 : "Banana",
+	3 : "Kiwi",
+	4 : "Grapes",
+	};
+  
+	//creating random integer function
+	function getRandomInt(min, max) 
+  {
+	const minCeiled = Math.ceil(min);
+	const maxFloored = Math.floor(max);
+	return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+  }
+  const n = getRandomInt(1,4);            //n: number | picks random number using the funtion 
+  const nr = fruit_table[n];              //nr: number response | response value for the number choosen above 
+  
+	if(request.method == 'GET')           //conditional statement
+	{
+	  return new Response ( "Random number" + " " + n + " " + "is a " + " " + nr );   
+	  }
+  
+	else {
+	  return new Response('Hello folks! Please use GET method', 
+	  {
+	   headers: {
+	 'Content-Type':'application/json',
+				},
+	  });
+	} 
+	}
+			  
+  };
+  
+	export {
+	  src_default as default
+  };
+  
+ 
+  
+>>>>>>> abe0613 (assignment 2)
